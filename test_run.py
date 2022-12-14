744d9be914662d5e97c09cda841bd13cb5d6d2ed
xiaotianxt <tianyp@pku.edu.cn>
2022-12-14 20:57:46 +0800

    test: add more test code

--- test_run.py
+++ test_run.py
@@ -2,15 +2,18 @@ import os
 import random
 import time
 
-start = time.time()
+times = []
 
-for i in range(10):
-    x, y = 115.5 + random.random(), 39.5 + random.random()
+for k in [25]:
+    start = time.time()
+    for i in range(5):
+        x, y = 115.5 + random.random(), 39.5 + random.random()
 
-    os.system(
-        f"python mr_query_nn.py -r local --k 10 --location '{x} {y}' output/index.txt > {i}.txt")
+        os.system(
+            f"python mr_query_nn.py -r local --k {k} --location '{x} {y}' output/index.txt > {i}.txt")
 
+    end = time.time()
 
-end = time.time()
+    times.append((end - start) / 5)
 
-print(end - start)
+print(*list(zip([3, 5, 10, 15, 25], times)), sep="\n")
